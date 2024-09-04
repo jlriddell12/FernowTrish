da4ad205188e7e6553791d80e8ae55bf735ae131
trishmiller3 <patrishacmiller@gmail.com>
2024-09-04 09:38:31 -0400

    Fixed Geo Map

--- FernowMaps.R
+++ FernowMaps.R
@@ -10,14 +10,22 @@ fernow <- st_transform(fernow, crs = st_crs("+proj=utm +zone=17 +datum=WGS84"))
 
 # WV geology
 
-geo <- st_read("C:\\Users\\patri\\OneDrive - Chatham University\\Trish_Summer2024\\GIS\\WV geo")
+geo <- st_read("C:\\Users\\patri\\OneDrive - Chatham University\\Trish_Summer2024\\GIS\\WV geo 2")
 
 geo <- st_transform(geo, crs = st_crs("+proj=utm +zone=17 +datum=WGS84"))
 
-ggplot(geo, fill = NAME)+
-  geom_sf()
+ggplot(geo)+
+  geom_sf(aes(fill = MAP_UNIT))
 
-## something is definitely not right there
+
+#Cutting out Fernow shape
+geo2 <- st_intersection(geo, fernow)
+
+ggplot(geo2)+
+  geom_sf(aes(fill = MAP_UNIT))+
+  scale_fill_manual("Map Unit", values = c('darkslategray2','lightgoldenrod','plum2','coral1','cornflowerblue','tan1','olivedrab2'))+
+  theme_minimal()
+
 
 # Watersheds
 
